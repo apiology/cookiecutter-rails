003f7fca81786daf904a4e19660af87063602487
Vince Broz <vince@broz.cc>
2024-08-25 11:27:45 -0400

    Add solargraph scan

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -47,6 +47,7 @@ if __name__ == '__main__':
         subprocess.check_call(['bundle', 'exec', 'rubocop', '-A'])
         subprocess.check_call(['git', 'add', '-A'])
         subprocess.check_call(['bundle', 'exec', 'yard', 'gems'])
+        subprocess.check_call(['bundle', 'exec', 'solargraph', 'scan'])
         subprocess.check_call(['bundle', 'exec', 'git', 'commit', '-m',
                                'rails new'])
 
