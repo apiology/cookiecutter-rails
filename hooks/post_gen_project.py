0376a22bf5ba86b7bffe5ed3b5b2e4ac1e144816
Vince Broz <vince@broz.cc>
2024-09-10 15:13:16 -0400

    Create 1Password entries for Rails master key

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     subprocess.check_call('./fix.sh')
     if os.environ.get('IN_COOKIECUTTER_PROJECT_UPGRADER', '0') == '1':
         os.environ['SKIP_GIT_CREATION'] = '1'
-        os.environ['SKIP_GITHUB_AND_CIRCLECI_CREATION'] = '1'
+        os.environ['SKIP_GITHUB_OP_AND_CIRCLECI_CREATION'] = '1'
 
     if os.environ.get('SKIP_GIT_CREATION', '0') != '1':
         # Don't run these non-idempotent things when in
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         subprocess.check_call(['bundle', 'exec', 'rubocop', '-A'])
         subprocess.check_call(['git', 'add', '-A'])
         subprocess.check_call(['make', 'clean-typecheck'])
-        subprocess.check_call(['bundle', 'exec', 'git', 'commit', '-m',
+        subprocess.check_call(['bundle', 'exec', 'git', 'commit', '--allow-empty', '-m',
                                'Initial commit from boilerplate'])
         subprocess.check_call(['gem', 'install', 'rails', '-v', '~> 7.0'])
         parent = os.path.dirname(PROJECT_DIRECTORY)
@@ -42,15 +42,26 @@ if __name__ == '__main__':
                                '--skip-test',
                                '--skip',
                                '{{cookiecutter.project_slug}}'], cwd=parent)
+        if os.environ.get('SKIP_GITHUB_OP_AND_CIRCLECI_CREATION', '0') != '1':
+            if subprocess.call(['op', 'item', 'get', '{{ cookiecutter.project_name }}']) != 0:
+                subprocess.check_call(['op', 'item', 'create',
+                                       '--category=Password',
+                                       '--title={{ cookiecutter.project_name }}'])
+            master_key_filename = os.path.join(PROJECT_DIRECTORY, 'config/master.key')
+            with open(master_key_filename) as f:
+                master_key = f.read().strip()
+                subprocess.check_call(['op', 'item', 'edit',
+                                       '{{ cookiecutter.project_name }}',
+                                       f"master key[password]={master_key}"])
 #        TODO: subprocess.check_call(['rails', 'generate', 'rspec:install'])
         subprocess.check_call(['make', 'bundle_install'])
         subprocess.check_call(['bundle', 'exec', 'rubocop', '-A'])
         subprocess.check_call(['git', 'add', '-A'])
         subprocess.check_call(['make', 'clean-typecheck'])
-        subprocess.check_call(['bundle', 'exec', 'git', 'commit', '-m',
+        subprocess.check_call(['bundle', 'exec', 'git', 'commit', '--allow-empty', '-m',
                                'rails new'])
 
-    if os.environ.get('SKIP_GITHUB_AND_CIRCLECI_CREATION', '0') != '1':
+    if os.environ.get('SKIP_GITHUB_OP_AND_CIRCLECI_CREATION', '0') != '1':
         if 'none' != '{{ cookiecutter.type_of_github_repo }}':
             if 'private' == '{{ cookiecutter.type_of_github_repo }}':
                 visibility_flag = '--private'
@@ -61,18 +72,22 @@ if __name__ == '__main__':
                                    'cookiecutter.type_of_github_repo: '
                                    '{{ cookiecutter.type_of_github_repo }}')
             description = "{{ cookiecutter.project_short_description.replace('\"', '\\\"') }}"
-            subprocess.check_call(['gh', 'repo', 'create',
-                                   visibility_flag,
-                                   '--description',
-                                   description,
-                                   '--source',
-                                   '.',
-                                   '{{ cookiecutter.github_username }}/'
-                                   '{{ cookiecutter.project_slug }}'])
-            subprocess.check_call(['gh', 'repo', 'edit',
-                                   '--allow-update-branch',
-                                   '--enable-auto-merge',
-                                   '--delete-branch-on-merge'])
+            # if repo already exists
+            if subprocess.call(['gh', 'repo', 'view',
+                                '{{ cookiecutter.github_username }}/'
+                                '{{ cookiecutter.project_slug }}']) != 0:
+                subprocess.check_call(['gh', 'repo', 'create',
+                                       visibility_flag,
+                                       '--description',
+                                       description,
+                                       '--source',
+                                       '.',
+                                       '{{ cookiecutter.github_username }}/'
+                                       '{{ cookiecutter.project_slug }}'])
+                subprocess.check_call(['gh', 'repo', 'edit',
+                                       '--allow-update-branch',
+                                       '--enable-auto-merge',
+                                       '--delete-branch-on-merge'])
             subprocess.check_call(['git', 'push'])
             subprocess.check_call(['circleci', 'follow'])
             subprocess.check_call(['git', 'branch', '--set-upstream-to=origin/main', 'main'])
