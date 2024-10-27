5b3dffd25223acb2b26deeb7d673ce983ef71d6f
Vince Broz <vince@broz.cc>
2024-10-27 12:42:12 -0400

    Create multiple full database entries in 1Password

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,6 +10,61 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def onepass_entry_exists(onepass_entry):
+    return subprocess.call(['op', 'item', 'get', onepass_entry]) == 0
+
+
+def create_onepass_entry(onepass_entry):
+    subprocess.check_call(['op', 'item', 'create',
+                           '--category=Login',
+                           f'--title={onepass_entry}'])
+
+def add_onepass_password_field(onepass_entry, password, password_field='password'):
+	subprocess.check_call(['op', 'item', 'edit',
+                           onepass_entry,
+                           f"{password_field}[password]={password}"])
+
+
+def add_onepass_field(onepass_entry, field_name, field_value):
+	subprocess.check_call(['op', 'item', 'edit',
+                           onepass_entry,
+                           f"{field_name}={field_value}"])
+
+
+def create_onepass_db(onepass_entry, password, database, password_field='password', port=5432, server=None):
+    if not onepass_entry_exists(onepass_entry):
+        create_onepass_entry(onepass_entry)
+        add_onepass_password_field(onepass_entry, password)
+        add_onepass_field(onepass_entry, 'port', port)
+        add_onepass_field(onepass_entry, 'database', database)
+        if server is not None:
+            add_onepass_field(onepass_entry, 'server', server)
+
+
+
+def random_password():
+    return subprocess.check_output(['openssl', 'rand', '-base64', '32']).decode().strip()
+
+
+def create_onepass_login(onepass_entry):
+    add_onepass_password_field(onepass_entry, password)
+
+
+def create_production_db_onepass_entry():
+    create_onepass_db("{{ cookiecutter.project_slug }} production database",
+                      password=random_password(),
+                      port=5432,
+                      database="{{ cookiecutter.project_slug }}")
+
+
+def create_docker_compose_db_onepass_entry(rails_env, port):
+    database = "{{ cookiecutter.project_slug.replace('-', '_') }}_" f"{rails_env}"
+    create_onepass_db('{{ cookiecutter.project_slug }} ' f"local {rails_env} docker-compose database - {database}",
+                      password=random_password(), port=port,
+                      server='localhost',
+                      database=database)
+
+
 if __name__ == '__main__':
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
@@ -35,24 +90,27 @@ if __name__ == '__main__':
         subprocess.check_call(['make', 'clean-typecheck'])
         subprocess.check_call(['bundle', 'exec', 'git', 'commit', '--allow-empty', '-m',
                                'Initial commit from boilerplate'])
-        subprocess.check_call(['gem', 'install', 'rails', '-v', '~> 7.0'])
         parent = os.path.dirname(PROJECT_DIRECTORY)
-        subprocess.check_call(['rails', 'new',
+        subprocess.check_call(['gem', 'install', 'rails', '-v', '~> 7.0'],
+                              cwd=parent)
+        subprocess.check_call(['rbenv', 'version'],
+                              cwd=parent)
+        subprocess.check_call(['rbenv', 'exec', 'rails', 'new',
                                '--database=postgresql',
                                '--skip-test',
                                '--skip',
                                '{{cookiecutter.project_slug}}'], cwd=parent)
         if os.environ.get('SKIP_GITHUB_OP_AND_CIRCLECI_CREATION', '0') != '1':
-            if subprocess.call(['op', 'item', 'get', '{{ cookiecutter.project_name }}']) != 0:
-                subprocess.check_call(['op', 'item', 'create',
-                                       '--category=Password',
-                                       '--title={{ cookiecutter.project_name }}'])
-            master_key_filename = os.path.join(PROJECT_DIRECTORY, 'config/master.key')
-            with open(master_key_filename) as f:
-                master_key = f.read().strip()
-                subprocess.check_call(['op', 'item', 'edit',
-                                       '{{ cookiecutter.project_name }}',
-                                       f"master key[password]={master_key}"])
+            main_onepass_entry = '{{ cookiecutter.project_name }}'
+            if not onepass_entry_exists(main_onepass_entry):
+                master_key_filename = os.path.join(PROJECT_DIRECTORY, 'config/master.key')
+                with open(master_key_filename) as f:
+                    master_key = f.read().strip()
+                create_onepass_entry(main_onepass_entry)
+                add_onepass_password_field(main_onepass_entry, master_key, password_field='master key')
+            create_docker_compose_db_onepass_entry('dev', port=5432)
+            create_docker_compose_db_onepass_entry('test', port=5433)
+            create_production_db_onepass_entry()
 #        TODO: subprocess.check_call(['rails', 'generate', 'rspec:install'])
         subprocess.check_call(['make', 'bundle_install'])
         subprocess.check_call(['bundle', 'exec', 'rubocop', '-A'])
