53f1b5302b25abf2a0ae26a842cba488cf2f70ab
Vince Broz <vince@broz.cc>
2024-11-29 16:55:15 -0500

    Post-process results after rails new

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import sys
 import tempfile
 from urllib.parse import urlparse
 
@@ -19,7 +20,7 @@ def run(*args, **kwargs):
     # keep both streams in the same place so that we can weave
     # together what happened on report instead of having them
     # dumped separately
-    subprocess.check_call(*args, stderr=subprocess.STDOUT, **kwargs)
+    subprocess.check_call(*args, stderr=subprocess.STDOUT, stdin=subprocess.DEVNULL, **kwargs)
 
 
 def remove_file(filepath):
@@ -78,6 +79,83 @@ def create_docker_compose_db_onepass_entry(rails_env, port):
                       database=database)
 
 
+def verify_backup_file(filename):
+    orig_filename = filename[:-1]
+    new_filename = filename
+    print("Verifying backup file", filename)
+
+    # if two files are identical, remove new one
+    with open(orig_filename) as f:
+        orig_contents = f.read()
+    with open(new_filename) as f:
+        new_contents = f.read()
+    if orig_contents == new_contents:
+        run(['rm', new_filename])
+        return
+
+    errmsg = f'Found a file ending in ~: {filename}'
+    # add the contents of old file and new file
+    # to the error message
+    errmsg += '\n'
+    errmsg += f'Old file: {orig_filename}\n'
+    with open(orig_filename) as f:
+        errmsg += f.read()
+    errmsg += '\n'
+    errmsg += f'New file: {new_filename}\n'
+    with open(new_filename) as f:
+        errmsg += f.read()
+    errmsg += f"Diff between files - save to {orig_filename}.patch\n"
+    # add diff to errmsg
+    errmsg += subprocess.getoutput(f'diff -u {orig_filename} {new_filename}')
+    raise RuntimeError(errmsg)
+
+
+def verify_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        if big_and_irrelevant_directory(root):
+            continue
+        for filename in files:
+            if filename.endswith('~'):
+                full_filename = os.path.join(root, filename)
+                verify_backup_file(full_filename)
+
+
+def big_and_irrelevant_directory(root):
+    return root.startswith('./.git') or root.startswith('./tmp/cache')
+
+
+def patch_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        if big_and_irrelevant_directory(root):
+            continue
+
+        for filename in files:
+            if filename.endswith('.patch'):
+                try:
+                    full_filename = os.path.join(root, filename)
+                    original_filename = full_filename[:-6]
+                    run(['patch', '-f', '-i', full_filename])
+                    # delete file
+                    run(['rm', full_filename])
+                except subprocess.CalledProcessError:
+                    print(f'Error patching using {full_filename}')
+                    with open(original_filename) as f:
+                        original_file_contents = f.read()
+                    print(f'File being patched:\n{original_file_contents}',
+                          flush=True, file=sys.stdout)
+                    orig_file = f"{original_filename}.orig"
+                    with open(orig_file) as f:
+                        orig_file_contents = f.read()
+                    print(f'Original file:\n{orig_file_contents}',
+                          flush=True, file=sys.stdout)
+                    rej_file = f"{original_filename}.rej"
+                    with open(rej_file) as f:
+                        rej_file_contents = f.read()
+                    print(f'Rejected hunk:\n{rej_file_contents}',
+                          flush=True, file=sys.stdout)
+                    raise
+
+
 if __name__ == '__main__':
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
@@ -86,29 +164,11 @@ if __name__ == '__main__':
     if 'Yes' != '{{ cookiecutter.use_checkoff }}':
         remove_file('config/initializers/checkoff.rb')
 
-    run('./fix.sh')
-    # update frequently security-flagged gems
-    if os.environ.get('IN_COOKIECUTTER_PROJECT_UPGRADER', '0') == '1':
-        os.environ['SKIP_GIT_CREATION'] = '1'
-        os.environ['SKIP_EXTERNAL'] = '1'
-        # os.environ['SKIP_RAILS_NEW'] = '1'
-
     if os.environ.get('SKIP_GIT_CREATION', '0') != '1':
         # Don't run these non-idempotent things when in
         # cookiecutter_project_upgrader, which will run this hook
         # multiple times over its lifetime.
         run(['git', 'init'])
-    run(['git', 'add', '-A'])
-    run(['bundle', 'exec', 'overcommit', '--install'])
-    run(['bundle', 'exec', 'overcommit', '--sign'])
-    run(['bundle', 'exec', 'overcommit', '--sign', 'pre-commit'])
-    run(['make', 'bundle_install'])
-    run(['bundle', 'exec', 'rubocop', '-A'])
-    run(['git', 'add', '-A'])
-    run(['make', 'build-typecheck'])
-    run(['bundle', 'exec', 'git', 'commit', '--allow-empty', '-m',
-         'Initial commit from boilerplate'])
-
     parent = os.path.dirname(PROJECT_DIRECTORY)
     # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html
     # Make this configurable?
@@ -129,6 +189,36 @@ if __name__ == '__main__':
             run(['gcp', '-R', '--backup',
                  os.path.join(tempdir, '{{cookiecutter.project_slug}}', '.'),
                  PROJECT_DIRECTORY])
+            revert = [
+                '.ruby-version',
+                '.rubocop.yml',
+                'README.md',
+                '.gitignore',
+            ]
+
+            # revert from the backup file, as these files don't have
+            # interesting upstream information for us
+            for filename in revert:
+                run(['mv', f'{filename}~', filename])
+
+            # find all files ending in .patch
+            patch_directory('.')
+
+            # error out if we find any files that end in '~' recursively
+            verify_directory('.')
+
+    run('./fix.sh')
+    run(['git', 'add', '-A'])
+    run(['bundle', 'exec', 'overcommit', '--install'])
+    run(['bundle', 'exec', 'overcommit', '--sign'])
+    run(['bundle', 'exec', 'overcommit', '--sign', 'pre-commit'])
+    run(['make', 'bundle_install'])
+    run(['bundle', 'exec', 'rubocop', '-A'])
+    run(['git', 'add', '-A'])
+    run(['make', 'build-typecheck'])
+    run(['bundle', 'exec', 'git', 'commit', '--allow-empty',
+         '-m', 'Initial commit from boilerplate'])
+
     if os.environ.get('SKIP_EXTERNAL', '0') != '1':
         main_onepass_entry = '{{ cookiecutter.project_name }}'
         if not onepass_entry_exists(main_onepass_entry):
@@ -142,16 +232,15 @@ if __name__ == '__main__':
         create_docker_compose_db_onepass_entry('dev', port=port_prefix + 2)
         create_docker_compose_db_onepass_entry('test', port=port_prefix + 3)
         create_production_db_onepass_entry()
-    run(['make', 'bundle_install'])
+    # update frequently security-flagged gems
     run(['bundle', 'update', '--conservative',
-         # 'rexml', # TODO
-         'rails', 'puma', 'nokogiri'])
+         'rexml', 'rails', 'puma', 'nokogiri'])
     run(['rails', 'g', 'rspec:install', '--skip'])
     run(['rails', 'importmap:install'])
     run(['rails', 'g', 'annotate:install', '--skip'])
     run(['bundle', 'exec', 'rubocop', '-A'])
     run(['git', 'add', '-A'])
-    # run(['make', 'build-typecheck']) # TODO
+    run(['make', 'build-typecheck'])
     run(['bundle', 'exec', 'git', 'commit', '--allow-empty', '-m',
          'rails new'])
 
@@ -161,7 +250,7 @@ if __name__ == '__main__':
         if '{{ cookiecutter.deploy_to_heroku }}' == 'yes':
             # run "heroku ps" to see if this already exists
             if subprocess.call(['heroku', 'ps', '-a', heroku_app_name]) != 0:
-                run(['heroku', 'create'])
+                run(['heroku', 'create', '{{ cookiecutter.project_slug }}'])
             # check for heroku_rediscloud
             if '{{ cookiecutter.heroku_rediscloud }}' == 'yes':
                 if subprocess.call(['heroku', 'config:get', 'REDISCLOUD_URL',
